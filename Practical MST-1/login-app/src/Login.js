76a21ae1c6174e06b5f3f88fad208b90bf663125
Joshisumit29 <joshisumit029@gmail.com>
2026-03-12 15:40:43 +0530

    added MST-1

---
+++ Practical MST-1/login-app/src/Login.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+
+function Login() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!email.includes("@") || !email.includes(".")) {
+      alert("Enter valid email");
+      return;
+    }
+
+    if (password.length < 6) {
+      alert("Password must be at least 6 characters");
+      return;
+    }
+
+    alert("Login Successful");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Login Form</h2>
+
+      <input
+        type="text"
+        placeholder="Enter Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <br /><br />
+
+      <input
+        type="password"
+        placeholder="Enter Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <br /><br />
+
+      <button type="submit">Login</button>
+    </form>
+  );
+}
+
+export default Login;
